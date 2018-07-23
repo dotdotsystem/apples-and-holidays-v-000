075bf2b527c1eeec185dc58154f8d0c4a6ed3fa2
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-23 22:42:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,14 +17,12 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+  holiday_supplies[:winter][:christmas] << "tree"
+  holiday_supplies[:winter][:new_years] << "glitter"
 end
 
 
@@ -61,10 +59,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
