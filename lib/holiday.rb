2d18eb37501543c1b1b779fa1f7b61f241765a60
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-23 22:44:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,8 +21,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_supplies[:winter][:christmas] << "tree"
-  holiday_supplies[:winter][:new_years] << "glitter"
+  holiday_supplies[:winter][:christmas] << "Balloons"
+  holiday_supplies[:winter][:new_years] << "Grill"
 end
 
 
